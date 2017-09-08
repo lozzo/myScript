934f80842b1a34065943cd02e10e897095357ef8
lozzo <wuyouget@gmail.com>
2017-09-08 22:55:55 +0800

    add apt-get update

--- install_env.sh
+++ install_env.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 sudo cp sources.list /etc/apt/sources.list
-sudo apt-get install zsh git htop tmux screen python-pip curl
+sudo apt-get update
+sudo apt-get install -y zsh git htop tmux screen python-pip curl
 sudo pip install virtualenv virtualenvwrapper
 git config --global user.name "lozzo"
 git config --global user.email "wuyouget@gmail.com"
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-cp zeta.zsh-theme /home/lozzow/.oh-my-zsh/themes
+cp zeta.zsh-theme /home/$USER/.oh-my-zsh/themes
 cp .zshrc ~ 
