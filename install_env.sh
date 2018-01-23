36f4fb7ac5c73944f5552b9d2dc3c8647560124b
lozzo <wuyouget@gmail.com>
2018-01-23 23:24:35 +0800

    增加pip豆瓣源

--- install_env.sh
+++ install_env.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-
+HOME=/home/$USER
 sudo cp sources.list /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get install -y zsh git htop tmux screen python-pip curl
+mkdir $HOME/.pip
+cp pip.conf $HOME/.pip/
 sudo pip install virtualenv virtualenvwrapper
 git config --global user.name "lozzo"
 git config --global user.email "wuyouget@gmail.com"
