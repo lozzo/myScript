8ae916d4c249cde6155cd3f12b97475489b072c2
lozzo <wuyouget@gmail.com>
2018-08-15 17:37:07 +0800

    go vim

--- install_env.sh
+++ install_env.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+set -e
+
 HOME=/home/$USER
-sudo cp sources.list /etc/apt/sources.list
 sudo apt-get update
-sudo apt-get install -y zsh git htop tmux screen python-pip curl
+sudo apt-get install -y zsh git htop tmux screen python-pip curl build-essential cmake python-dev
 mkdir $HOME/.pip
 cp pip.conf $HOME/.pip/
 sudo pip install virtualenv virtualenvwrapper
 git config --global user.name "lozzo"
 git config --global user.email "wuyouget@gmail.com"
+mkdir ~/.vim/bundle 
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp .vimrc ~/.vimrc
+mkdir ~/go 
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 cp zeta.zsh-theme /home/$USER/.oh-my-zsh/themes
 cp .zshrc ~ 
