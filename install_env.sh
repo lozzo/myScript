86b784bc2fb37d321915fdc1d01ed642d69beb03
lozzo <wuyouget@gmail.com>
2018-11-22 09:48:17 +0800

    增加tmux2.8 安装

--- install_env.sh
+++ install_env.sh
@@ -3,7 +3,7 @@ set -e
 
 HOME=/home/$USER
 sudo apt-get update
-sudo apt-get install -y zsh git htop tmux screen python-pip curl build-essential cmake python-dev
+sudo apt-get install -y zsh git htop screen python-pip curl build-essential cmake python-dev libevent-dev
 mkdir $HOME/.pip
 cp pip.conf $HOME/.pip/
 sudo pip install virtualenv virtualenvwrapper
@@ -16,3 +16,9 @@ mkdir ~/go
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 cp zeta.zsh-theme /home/$USER/.oh-my-zsh/themes
 cp .zshrc ~ 
+wget https://github.com/tmux/tmux/releases/download/2.8/tmux-2.8.tar.gz -O /tmp/tmux.tgz
+mkdir -p /tmp/build-tmux
+tar -zxvf /tmp/tmux.tgz -C /tmp/build-tmux 
+cd /tmp/build-tmux/tmux-2.8 && ./configure && make
+sudo make install
+
